bfb50e125763e78ff5ac1c2c358f9413c32ad185
jashmehta3300 <jashmehta3300@gmail.com>
2020-05-17 21:07:54 +0530

    connected redux to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       Hello World!
     </div>
+    </Provider>
   );
 }
 
